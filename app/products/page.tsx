db0941070168de826d48263ee298ca182b2e0994
Srinu126 <srinurachakonda126@gmail.com>
2025-01-15 09:45:33 -0500

    Dynamic routes

---
+++ app/products/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ProductsPage = () => {
+  return (
+    <div>
+        <h1>List of products</h1>
+        <ul>
+            <li>Product One</li>
+            <li>Product Two</li>
+            <li>Product Three</li>
+        </ul>
+    </div>
+  )
+}
+
+export default ProductsPage
