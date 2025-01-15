056b94a4a146e165e127fe7956c6bef45c58af8d
Srinu126 <srinurachakonda126@gmail.com>
2025-01-15 11:33:06 -0500

    Not Found page

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+        <h2>Page Not Found</h2>
+        <p>Could not find requested resources</p>
+    </div>
+  )
+}
+
+export default NotFound
--- app/products/[productId]/reviews/[reviewId]/page.tsx
+++ app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,12 @@
+import { notFound } from 'next/navigation';
 import React from 'react'
 
 const ReviewDetails = async({params}: {params: Promise<{productId: string; reviewId: string}>}) => {
     const productId = (await params).productId;
   const reviewId = (await params).reviewId;
+  if(parseInt(reviewId)>1000){
+    notFound();
+  }
   return (
     <div>
         <h1>Review Details for product-{productId} with review - {reviewId}</h1>
