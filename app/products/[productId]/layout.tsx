acf782aaebfc3a2a4136754ab9aa671f2b1bbe4d
Srinu126 <srinurachakonda126@gmail.com>
2025-01-16 09:50:18 -0500

    nested layouts

---
+++ app/products/[productId]/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProductLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      ProductLayout
+      {children}
+    </div>
+  );
+};
+
+export default ProductLayout;
