0eef0d15298c9f4352ea68f761cc4fc9b7d2f5a7
Srinu126 <srinurachakonda126@gmail.com>
2025-01-16 10:58:39 -0500

    title metadata

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next"
 
 //static metadata object
 export const metadata: Metadata = {
-    title: "Login Page",
+    // title: "Login Page",
     description: "Login Page Description"
 }
 
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,4 +1,11 @@
+import type { Metadata } from 'next'
 import React from 'react'
+
+export const metadata: Metadata = {
+  title: {
+    absolute:"Blog Page"
+  }
+}
 
 const Blog = () => {
   return (
