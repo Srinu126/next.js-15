951ac580aa7532c08c022525160139fc91c9e2c6
Srinu126 <srinurachakonda126@gmail.com>
2025-01-16 10:46:24 -0500

    routingmetadata

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,3 +1,12 @@
+import type { Metadata } from "next"
+
+//static metadata object
+export const metadata: Metadata = {
+    title: "Login Page",
+    description: "Login Page Description"
+}
+
+
 export default function Login(){
     return(
         <h1>Login Page</h1>
--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import type { Metadata } from "next";
+type Props = {
+  params: Promise<{ productId: string }>;
+};
 
-const ProductDetails = async({params}: {params: Promise<{productId: string}>}) => {
-    const productId = (await params).productId
+//Dynamic Metadata
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const id = (await params).productId;
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iPhone ${id}`);
+    }, 100);
+  });
+  return {
+    title: `Product ${title}`,
+  };
+};
+
+const ProductDetails = async ({ params }: Props) => {
+  const productId = (await params).productId;
   return (
     <div>
-        <h1>Details of the product - {productId}</h1>
+      <h1>Details of the product - {productId}</h1>
     </div>
-  )
-}
+  );
+};
 
-export default ProductDetails
+export default ProductDetails;
