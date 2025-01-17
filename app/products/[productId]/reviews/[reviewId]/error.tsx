528d4cb95fb6921100c767dd2529cf073f12afa3
Srinu126 <srinurachakonda126@gmail.com>
2025-01-16 19:50:39 -0500

    recovering from errors

--- app/products/[productId]/reviews/[reviewId]/error.tsx
+++ app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,7 +1,28 @@
 "use client";
 
-const ErrorBoundary = ({ error }: { error: Error }) => {
-  return <div>ErrorBoundary - {error.message}</div>;
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
+
+const ErrorBoundary = ({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) => {
+  const router = useRouter();
+  const reload = () => {
+    startTransition(() => {
+      router.refresh();
+      reset();
+    });
+  };
+  return (
+    <div>
+      <p>ErrorBoundary - {error.message}</p>
+      <button onClick={reload}>Try Again</button>
+    </div>
+  );
 };
 
 export default ErrorBoundary;
