25fef9ffce7d4b5046af044dec0339e913d41391
Srinu126 <srinurachakonda126@gmail.com>
2025-01-25 10:15:24 -0500

    cookies in route handlers

--- app/profile/api/route.ts
+++ app/profile/api/route.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import type { NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 
 export async function GET(request: NextRequest) {
   //   const requestHeaders = new Headers(request.headers);
@@ -8,9 +8,18 @@ export async function GET(request: NextRequest) {
 
   const headerList = await headers();
   console.log(headerList.get("Authorization"));
+
+  const theme = request.cookies.get("theme");
+  console.log(theme);
+
+  const cookieStore = await cookies();
+  cookieStore.set("resultsPerPage", "20");
+
+  console.log(cookieStore.get("resultsPerPage"));
   return new Response("<h1>Profile API Data</h1>", {
     headers: {
       "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark",
     },
   });
 }
